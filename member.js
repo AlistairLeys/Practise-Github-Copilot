eef3230e07750cb2aa0ae48acc486e82cbd09810
AlistairLeys <130834304+AlistairLeys@users.noreply.github.com>
2024-08-25 18:54:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var skill = this.skill;
+  if (skill === 'JavaScript') {
+    console.log('I am good at JavaScript');
+  } else {
+    console.log('I am good at something else');
+  }
+}
